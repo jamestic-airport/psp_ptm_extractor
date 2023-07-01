8ed67df25352aba582aca46176cdecf2e0b77522
James R <j.ryu.rosse@gmail.com>
2023-07-01 15:32:04 +1000

    updated for integration with extract.py

--- PSP_PTM_extractor.py
+++ PSP_PTM_extractor.py
@@ -9,67 +9,82 @@ import pandas as pd
 # Reads an excel spreadsheet of all ribosomal proteins. Creates a key value pair of all
 # PSP codes to their matching single gene names. This is used to generate cleaner output
 # for the final tsv table
-def get_psp_code_conversion_dict():
+def get_uniprot_names():
 
     df = pd.read_excel('Human_Ribosome_List.xlsx')
-    rp_data = df.to_dict(orient='records')
+    proteins = df.to_dict(orient='records')
     code_dict = dict()
-    for row in rp_data:
-        psp_code = str(row['PSP Code'])
-        gene_name = row['Single Gene Name']
-        code_dict[psp_code.strip()] = gene_name.strip()
-
+    for protein in proteins:
+        psp_code = str(protein['PSP Code']).strip()
+        entry = protein['Entry'].strip()
+        entry_name = protein['Entry Name'].strip()
+        code_dict[psp_code] = [entry, entry_name]
     return code_dict
 
 
 # Read protein list from proteins.txt
-# PSP doesn't like having more than 49 entries searched at once
-with open('proteins.txt', 'r') as file:
+filepath = 'short_proteins.txt'
+with open(filepath, 'r') as file:
     protein_list = file.readlines()
-    # Strip any whitespace characters (including newline characters) from each line
     protein_list = [line.strip() for line in protein_list]
 file.close
 
-psp_conversion_dict = get_psp_code_conversion_dict()
+uniprot_names = get_uniprot_names()
+#pprint.pprint(uniprot_names)
 ptm_data = list()
 
 for protein in protein_list:
-    url = f"https://www.phosphosite.org/proteinAction?id={protein}&showAllSites=true" # Retrieve PTM info from URL
-    response = requests.get(url) # Send a GET request to the URL and retrieve the HTML content
+    # MODIFICATION RETRIEVAL. For each protein, extract information on every modification found on that protein. 
+    # Each modification is represented as a dictionary
+    url = f"https://www.phosphosite.org/proteinAction?id={protein}&showAllSites=true" 
+    response = requests.get(url) 
     html = response.content
-    soup = BeautifulSoup(html, "html.parser") # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html, "html.parser") 
 
-    # PTMS are stored in the div class 'data container' under the param id 'PTMsites'. Extract PMT information
-    # into a list of dicts
-    ptms_param = soup.find('param', id = 'PTMsites')
+    ptms_param = soup.find('param', id = 'PTMsites') # PTMS are stored in the div class 'data container' under the param id 'PTMsites'. 
     if ptms_param:
         ptms = ptms_param['value'] # This is a json string
-        ptms = json.loads(ptms) # Converts json string to list of dicts
+        modifications = json.loads(ptms) # Converts json string to list of dicts
     else:
         print(f'No PTMS for this ribosomal protein {protein}')
         continue
 
+    # DATA EXTRACTION AND TABLE CREATION. Relevant information about each modification is extracted and
+    # outputted as a pandas data frame.
+    mod_type_list = []
+    mod_pos_list = []
+    paper_num_list = []
+
+    mod_count = 0
+    for mod in modifications:
+        mod_type_list.append(mod['MODIFICATION'])
+        mod_pos_list.append(mod['POS'])
+        paper_num_list.append(mod['REF'])
+        mod_count += 1
+  
+    df = pd.DataFrame({
+        'Accession': uniprot_names[protein][0],
+        'Entry': uniprot_names[protein][1],
+        'Mod Type': mod_type_list,
+        'Mod Position': mod_pos_list,
+        'Localization Probability': 'none',
+        'No of References': paper_num_list,
+        'Database': 'PSP' 
+    })
+
+    pd.set_option('display.max_rows', None)   # Set option to display all rows
+    pd.set_option('display.max_columns', None)   # Set option to display all columns    
+    pd.set_option('display.width', None)
+    print(df)
+
     # Cleaning up NMER string from 'EKVKVNG<font color=#993333>k</font>TGNLGNV' to 'EKVKVNGkTGNLGNV'
-    for fields in ptms:
-        nmer_string = fields.get('NMER')
+    for mod in modifications:
+        nmer_string = mod.get('NMER')
         new_string = nmer_string.replace("<font color=#993333>", "").replace("</font>", "")
-        fields['NMER'] = new_string
+        mod['NMER'] = new_string
+        mod['PROTEIN'] = uniprot_names.get(protein)
+    ptm_data.extend(modifications)
 
-        fields['PROTEIN'] = psp_conversion_dict.get(protein)
-
-    ptm_data.extend(ptms)
-
-# Headers are 'HTP', 'ID', 'LTP', 'MODCOLOR', 'MODIFICATION', 'NMER', 'POS', 'PUBMED', 'REF', 'allNum'
-headers = ['PROTEIN', 'MODIFICATION', 'ID', 'NMER', 'REF', 'HTP', 'LTP', 'PUBMED', 'allNum']
-
-
-# Writing into output.tsv
-with open('output.tsv', 'w', newline='') as file:
-    writer = csv.DictWriter(file, delimiter='\t', fieldnames=headers) # Create a TSV writer object
-    writer.writeheader() # Write the header row to the file
-    for row in ptm_data: # Write each dictionary in the list to a row in the file
-        writer.writerow({key: value for key, value in row.items() if key in headers})
-file.close()
 
 
 
